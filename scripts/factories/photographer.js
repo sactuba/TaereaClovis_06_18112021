0349186058c9af3715e9733958812a507babd3b4
sactuba <taloamn85@gmail.com>
2021-11-29 20:23:13 +0100

    Mise a jour

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,32 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+  function photographerFactory(data) {
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        const  a = document.createElement( 'a' );
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const span = document.createElement( 'span' );  
+        const p = document.createElement( 'p' );
+        const prix = document.createElement( 'p' );
+        prix.className = "prix";
+        img.setAttribute('href', "photographer.html"); 
+        img.setAttribute("src", picture)
+        span.textContent = country + ', ' + city ;
+        p.textContent = tagline;
+        prix.textContent = price + ' €/jour';
         h2.textContent = name;
+        a.appendChild(article);
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(span);
+        article.appendChild(p);
+        article.appendChild(prix);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, portrait,city, country, tagline, price, getUserCardDOM }
 }
+
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,14 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+
+
+function photoPhotographer(namePhotographer){
+    const chemin = "/assets/Sample Photos/" + namePhotographer;
+    let contenue = document.getElementById("contenue_id");
+
+    for (var i = 0; i < photo.length; i++) {
+        const photo = document.createElement('img');
+        contenue.appendChild(photo);
+    }
+
+}
