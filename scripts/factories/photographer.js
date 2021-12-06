62b8567cfc5937efb6a06e23dcb627bf555b67ff
sactuba <taloamn85@gmail.com>
2021-12-06 19:02:18 +0100

    mise a jour de la fonction pour afficher les photgraphe sur la page d'acceuill

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,32 +1,69 @@
-  function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+class photographerFactory{
+  constructor(photographer) {
+    this.name = photographer.name
+    this.id = photographer.id
+    this.city = photographer.city
+    this.country = photographer.country
+    this.tageline = photographer.tageline
+    this.price = photographer.price
+    this.portrait = photographer.portrait 
+  }
+  
+/*   get name() {
+    return this.name
+  }
+  get city() {
+    return this.city
+  }
+  get country() {
+    return this_country
+  }
+  get tageline() {
+    return this.tageline
+  }
+  get price() {
+    return this.price
+  }
+  get portrait() {
+    return this.portrait
+  } */
 
-    function getUserCardDOM() {
-        const  a = document.createElement( 'a' );
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        const h2 = document.createElement( 'h2' );
-        const span = document.createElement( 'span' );  
-        const p = document.createElement( 'p' );
-        const prix = document.createElement( 'p' );
-        prix.className = "prix";
-        img.setAttribute('href', "photographer.html"); 
-        img.setAttribute("src", picture)
-        span.textContent = country + ', ' + city ;
-        p.textContent = tagline;
-        prix.textContent = price + ' €/jour';
-        h2.textContent = name;
-        a.appendChild(article);
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(span);
-        article.appendChild(p);
-        article.appendChild(prix);
-
-        return (article);
-    }
-    return { name, portrait,city, country, tagline, price, getUserCardDOM }
+   getUserCardDOM() {
+    const picture = `assets/photographers/${this.portrait}`;
+    const  a = document.createElement( 'a' );
+    const article = document.createElement( 'article' );
+    const img = document.createElement( 'img' );
+    const h2 = document.createElement( 'h2' );
+    const span = document.createElement( 'span' );  
+    const p = document.createElement( 'p' );
+    const prix = document.createElement( 'p' );
+    prix.className = "prix";
+    img.setAttribute('href', "photographer.html"); 
+    img.setAttribute("src", picture)
+    span.textContent = this.country + ', ' + this.city ;
+    p.textContent = this.tagline;
+    prix.textContent = this.price + ' €/jour';
+    h2.textContent = this.name;
+    a.appendChild(article);
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(span); 
+    article.appendChild(p);
+    article.appendChild(prix);
+/*     const card = `
+    <a href="#">
+    <article>
+    <img src = ${picture}></img>
+    <h2>{this.name}</h2>
+    <span>${this.country} , ${this.city}</span>
+     <p>${this.tageline}</p>
+     <p>${this.price} €/jour</p>
+    </article>
+    </a>; */
+    return (article); 
 }
+}
+
+
 
